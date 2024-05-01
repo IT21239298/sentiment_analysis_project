97b5315d91a649f6111fa25cc53ba3c5569997c8
Sithum <99238978+IT21239298@users.noreply.github.com>
2024-05-01 16:18:59 +0530

    modify app.py

--- app.py
+++ app.py
@@ -1,38 +1,35 @@
-from flask import Flask, render_template,request, redirect
+from flask import Flask, render_template, request, redirect, jsonify
+from flask_cors import CORS
 from helper import preprocessing, vectorizer, get_prediction
 
-
 app = Flask(__name__)
+CORS(app)
 
-data = dict()
-reviews = []
-positive = 0
-negative = 0
+data = {'reviews': [], 'positive': 0, 'negative': 0}
 
 @app.route("/")
 def index():
-    data['reviews'] = reviews
-    data['positive'] = positive
-    data['negative'] = negative
-    return render_template('index.html', data=data)
+    return redirect("http://localhost:3000/anaysys")
 
-@app.route("/", methods = ['post'])
+@app.route("/anaysys")
+def analysis():
+    return jsonify(data)
+
+@app.route("/", methods=['POST'])
 def my_post():
     text = request.form['text']
     preprocessed_txt = preprocessing(text)
     vectorized_txt = vectorizer(preprocessed_txt)
     prediction = get_prediction(vectorized_txt)
 
+    global data
     if prediction == 'negative':
-        global negative
-        negative += 1
+        data['negative'] += 1
     else:
-        global positive
-        positive += 1
+        data['positive'] += 1
     
-    reviews.insert(0, text)
-    return redirect(request.url)
-    
+    data['reviews'].insert(0, {'text': text, 'sentiment': prediction})  # Include sentiment in the reviews
+    return jsonify(data)
 
 if __name__ == "__main__":
     app.run()
